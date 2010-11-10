8d3d014f29b209f82b9cb3a1254fe4c5b00f7360
Markus Schlegel <mail@markus-schlegel.com>
2010-11-10 19:25:26 +0100

    Flattr-Button zum Wohle der Soundqualitaet hinzugefuegt.

--- header.php
+++ header.php
@@ -84,7 +84,8 @@ textarea, input[type=text] { width:100% }
 <p>
 	<small>
 		Alle Inhalte stehen, sofern nicht anders vermerkt, unter einer <a title="Namensnennung-Weitergabe unter gleichen Bedingungen 3.0 Deutschland" href="http://creativecommons.org/licenses/by-sa/3.0/de/">CC-BY-SA-Lizenz</a>.
-		Musik: <a href="http://www.jamendo.com/en/album/6746">Kursed - Abstract</a>
+		Musik: <a href="http://www.jamendo.com/en/album/6746">Kursed - Abstract</a> Direkt-Flattr zum Wohle der Audioqualität: <a class="FlattrButton" style="display:none;" rev="flattr;button:compact;"
+href="http://workingdraft.de"></a>
 	</small>
 </p>
 
